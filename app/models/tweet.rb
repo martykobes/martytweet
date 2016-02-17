5ba86a099d9136ca18bd5a397587b20b0772e52d
Marty Kowbel <mekobes44@yahoo.com>
2016-02-17 07:17:46 -0500

    added profiles

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates :message, presence: true, length: { maximum: 140,
+                                                too_long: 'A tweet is only 140 max. Everybody knows that!' }
   belongs_to :user
 end
